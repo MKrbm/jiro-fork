519e26df793acb8725ea2fa6b4c2953e0040a1cd
Futty93 <kazuki.futawatari.s5@dc.tohoku.ac.jp>
2024-04-11 11:31:50 +0900

    add first mock up and Header Design

---
+++ app/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import Header from "@/app/ui/header";
+
+export default function MainComponent() {
+  const [showCurrentLocationOption, setShowCurrentLocationOption] = React
+    .useState(false);
+
+  return (
+    <div className="relative bg-white">
+      <Header />
+      <div className="relative">
+        <Image
+          src="/ai-img.jpg"
+          width={1024}
+          height={1024}
+          className="block w-full h-[500px] object-cover"
+          alt="Screenshots of the dashboard project showing desktop version"
+        />
+        <div className="absolute w-full text-center top-[50%] translate-y-[-50%]">
+          <h1 className="font-roboto text-5xl text-white">
+            Agents. Tours. Loans. Homes.
+          </h1>
+          <div className="mt-4 relative">
+            <input
+              type="text"
+              name="search"
+              placeholder="Enter an address, neighborhood, city, or ZIP code"
+              className="pl-4 pr-10 py-2 w-[400px] md:w-[600px] lg:w-[800px] rounded-full shadow-lg focus:outline-none"
+              onFocus={() => setShowCurrentLocationOption(true)}
+              onBlur={() => setShowCurrentLocationOption(false)}
+            />
+            <i className="fa fa-search text-blue-500 absolute top-2 right-2">
+            </i>
+            {showCurrentLocationOption && (
+              <div className="absolute w-full bg-white text-left px-4 py-2 mt-1 shadow-lg text-black">
+                Use current location
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/ui/header.tsx
@@ -0,0 +1,126 @@
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+import clsx from "clsx";
+import { useState } from "react";
+
+const links = [
+  { name: "Buy", href: "/buy" },
+  { name: "Rent", href: "/rent" },
+  { name: "Sell", href: "/sell" },
+  { name: "Loan", href: "/loan" },
+  { name: "Agents", href: "/agents" },
+  { name: "Manege Rentals", href: "/manege-rentals" },
+  { name: "Advertise", href: "/advertise" },
+  { name: "Help", href: "/help" },
+  { name: "Sign In", href: "/sing-in" },
+];
+
+export default function Header() {
+  const pathname = usePathname();
+  const [showMenu, setShowMenu] = useState(false);
+
+  return (
+    <header className="bg-white py-4 px-6">
+      {/* header when screen size is middle or higher */}
+      <div className="hidden md:flex">
+        <div className="flex-grow">
+          <div className="flex items-center">
+            <div className="flex justify-start items-center">
+              {links.slice(0, 5).map((link) => (
+                <Link
+                  key={link.name}
+                  href={link.href}
+                  className={clsx(
+                    "flex text-black h-[48px] items-center justify-center gap-2 rounded-md p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600",
+                    {
+                      "bg-sky-100 text-blue-600": pathname === link.href,
+                    },
+                  )}
+                >
+                  <p className="hidden md:block">{link.name}</p>
+                </Link>
+              ))}
+            </div>
+            <div className="flex-grow" />
+          </div>
+        </div>
+        <div className="flex-grow-0">
+          <Link href="/">
+            <Image
+              src="/jiro-housing.svg"
+              width={1000}
+              height={300}
+              alt="Logo"
+              className="h-12 w-auto cursor-pointer"
+            />
+          </Link>
+        </div>
+        <div className="flex-grow">
+          <div className="flex justify-end items-center">
+            {links.slice(5).map((link) => (
+              <Link
+                key={link.name}
+                href={link.href}
+                className={clsx(
+                  "flex text-black h-[48px] items-center justify-center gap-2 rounded-md p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600",
+                  {
+                    "bg-sky-100 text-blue-600": pathname === link.href,
+                  },
+                )}
+              >
+                <p className="hidden md:block">{link.name}</p>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      {/* header when screen size is small */}
+      <div className="md:hidden">
+        <div className="container mx-auto flex justify-between items-center">
+          <button
+            className="text-black"
+            onClick={() => setShowMenu(!showMenu)}
+          >
+            ☰
+          </button>
+          <div className="flex-grow-0">
+            <Link href="/">
+              <Image
+                src="/jiro-housing.svg"
+                width={1000}
+                height={300}
+                alt="Logo"
+                className="h-12 w-auto cursor-pointer"
+              />
+            </Link>
+          </div>
+          {links.slice(-1).map((link) => (
+            <Link
+              key={link.name}
+              href={link.href}
+              className="text-black hover:text-blue-600"
+            >
+              <p>{link.name}</p>
+            </Link>
+          ))}
+        </div>
+        {/* Mobile Menu */}
+        {showMenu && (
+          <div className="fixed top-16 left-6 z-50 bg-white py-4 px-6 shadow-md">
+            {links.slice(0, 8).map((link) => (
+              <Link
+                key={link.name}
+                href={link.href}
+                className="block text-black hover:text-blue-600 py-2"
+              >
+                <p>{link.name}</p>
+              </Link>
+            ))}
+          </div>
+        )}
+      </div>
+    </header>
+  );
+}
