81039e42f4e34de9dee8e2f549b710037fcdd58b
Futty93 <kazuki.futawatari.s5@dc.tohoku.ac.jp>
2024-06-07 10:16:42 +0900

    edit to rent page can display

--- app/homes/for-rent/page.tsx
+++ app/homes/for-rent/page.tsx
@@ -1,31 +1,80 @@
-import React from "react";
-import { PropertyCard } from "@/app/ui/homes/property-card";
-import { Box, Container, Typography } from "@mui/material";
+"use client";
+import React, { useState } from "react";
+import { PropertyCardArea } from "@/app/ui/homes/property-card-area";
+import { Box, Container, Button, useMediaQuery, useTheme } from "@mui/material";
+import { MapDetails } from '@/components/gmap/types/Camera';
 import Map from "@/components/gmap/Map";
 
-export const metadata = {
-  title: 'Rent',
-};
+// export const metadata = {
+//   title: 'Rent',
+// };
 
 export default function Page() {
+  const [isFullScreen, setIsFullScreen] = useState(true);
+  const theme = useTheme();
+  const isXs = useMediaQuery(theme.breakpoints.down('sm')); // smartphones
+  const isSm = useMediaQuery(theme.breakpoints.between('sm', 'md')); // tablets
+  const isMdUp = useMediaQuery(theme.breakpoints.up('md')); // larger screens
+
+  const mapDetails: MapDetails = {
+    center_lat: 35.6803,
+    center_lng: 139.7690,
+    width: 1,
+    height: 1,
+  };
+
+  const toggleFullScreen = () => {
+    setIsFullScreen(!isFullScreen);
+  };
   return (
-    <Container disableGutters maxWidth={false} sx={{height: 'calc(100Vh-128px)', display: 'flex', backgroundColor: 'white' }}>
+    <Container disableGutters maxWidth={false} sx={{ height: 'calc(100vh - 64px)', position: 'relative', backgroundColor: 'white', overflow: 'hidden' }}>
+    {/* Flexbox Container for Map and PropertyCardArea */}
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: isMdUp ? 'row-reverse' : 'column', // Row for isMdUp, column otherwise
+        height: '100%',
+      }}
+    >
+      {/* Map */}
       <Box
         sx={{
-          flex: 1,
-          display: { xs: 'none', md: 'flex' },
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: 'gray.200',
-          position: 'relative',
+          flex: 1, // Grow to fill available space
+          position: isMdUp ? '' : 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          overflow: 'hidden',
         }}
       >
         <Map center_lat={35.6803} center_lng={139.7690} width={1} height={1} />
       </Box>
+      
+      {/* PropertyCardArea */}
+      <PropertyCardArea mapDetails={mapDetails} isFullScreen={isFullScreen} />
+    </Box>
 
-      <Box sx={{ width: { xs: '100%', md: '320px', lg: '640px' }}}>
-        <PropertyCard />
-      </Box>
-    </Container>
+    {/* Map or List Togle Button */}
+    {(isXs || isSm) && (
+      <Button
+      variant="contained"
+      size="large"
+      onClick={toggleFullScreen}
+      sx={{
+        position: 'fixed',
+        width: '70%',
+        maxWidth: 400,
+        bottom: 64,
+        left: '50%',
+        transform: 'translateX(-50%)', // ボタンを中央に配置
+        zIndex: 9999, // zIndexの値を大きく修正
+      }}
+    >
+      {isFullScreen ? 'Map' : 'List'}
+    </Button>
+    
+      )}
+  </Container>
   );
 }
