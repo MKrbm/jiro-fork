75a4ef2e3e91a001a27fcf1aa420bbb599e74619
Futty93 <kazuki.futawatari.s5@dc.tohoku.ac.jp>
2024-06-07 17:07:35 +0900

    Rent page design change

--- app/homes/for-sale/page.tsx
+++ app/homes/for-sale/page.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { PropertyCardArea } from "@/app/ui/homes/property-card-area";
 import { Box, Container, Button, useMediaQuery, useTheme } from "@mui/material";
 import { MapDetails } from '@/components/gmap/types/Camera';
+import Map from "@/components/gmap/Map";
 
 // export const metadata = {
 //   title: 'Buy',
@@ -48,11 +49,12 @@ export default function Page() {
             overflow: 'hidden',
           }}
         >
-          <iframe
+          {/* <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3172.3321824596837!2d-122.01384291346217!3d37.334643700160434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fb596e9e188fd%3A0x3b0d8391510688f0!2z44Ki44OD44OX44Or44O744OR44O844Kv!5e0!3m2!1sja!2sus!4v1712887239111!5m2!1sja!2sus"
           style={{ width: '100%', height: '100%' }}
           loading="lazy"
-        ></iframe>
+        ></iframe> */}
+          <Map center_lat={35.6803} center_lng={139.7690} width={1} height={1} />
         </Box>
         
         {/* PropertyCardArea */}
